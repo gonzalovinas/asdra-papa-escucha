6498ab640879d759dd16590608f2866931e452ee
Gonzalo Viñas <gonzalo.vinas@gmail.com>
2014-06-24 01:15:22 -0300

    .

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,18 @@ class WelcomeController < ApplicationController
 
   def index
   end
+
+  def resumen
+    @mensajes_novedades = MensajesNovedades.last(5)
+    render :action => "resumen", :layout=> false
+  end
+
+  def alta_nueva_entrevista
+    render :action => "alta_nueva_entrevista", :layout=> false
+  end
+
+  def do_alta_nueva_entrevista
+    render :json => {:status=>"Mensaje Recibido"}
+  end
+
 end
---
+++ app/models/mensajes_novedades.rb
@@ -0,0 +1,3 @@
+class MensajesNovedades < ActiveRecord::Base
+  self.table_name = 'mensajes_novedades'
+end
