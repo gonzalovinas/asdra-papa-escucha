63cec9e2187c8fce11298cce1df1e4ba9705294e
Gonzalo Viñas <gonzalo.vinas@gmail.com>
2014-06-25 10:40:58 -0300

    .

--- app/controllers/entrevista_controller.rb
+++ app/controllers/entrevista_controller.rb
@@ -11,11 +11,40 @@ class EntrevistaController < ApplicationController
   def do_alta_nueva_entrevista
 
     ne = Entrevista.new
-    ne.fecha = params[:fecha_llamada]
-    ne.lugar = params[:lugar]
+
+    ne.fecha_llamada    = params[:fecha_llamada]
+    ne.fecha_entrevista = params[:fecha_llamada]
+    ne.lugar            = params[:lugar]
+    ne.mama_escucha     = params[:mama_escucha]
+
+    ne.papa_escucha     = params[:papa_escucha]
+    ne.como_supo        = params[:como_supo]
+    ne.papa_nombre_apellido = params[:papa_nombre_apellido]
+    ne.papa_edad        = params[:papa_edad]
+    ne.papa_ocupacion   = params[:papa_ocupacion]
+    ne.papa_domicilio   = params[:papa_domicilio]
+    ne.papa_telefonos   = params[:papa_telefonos]
+    ne.papa_correo      = params[:papa_correo]
+    ne.mama_nombre_apellido = params[:mama_nombre_apellido]
+    ne.mama_edad        = params[:mama_edad]
+    ne.mama_ocupacion   = params[:mama_ocupacion]
+    ne.mama_domicilio   = params[:mama_domicilio]
+    ne.mama_telefonos   = params[:mama_telefonos]
+    ne.mama_correo      = params[:mama_correo]
+    ne.sexo             = params[:sexo]
+    ne.fecha_nacimiento = params[:fecha_nacimiento]
+    ne.institucion_nacimiento = params[:institucion_nacimiento]
+    ne.observacion_institucion= params[:observacion_institucion]
+    ne.meses_nacimiento = params[:meses_nacimiento]
+    ne.parto_normal     = params[:parto_normal]
+    ne.sabia_sdown      = params[:sabia_sdown]
+    ne.recepcion_flia   = params[:recepcion_flia]
+    ne.patologia_agregada = params[:patologia_agregada]
+    ne.observaciones    = params[:observaciones]
+
     ne.save
 
-    render :json => {:status=>"OK"}
+    render :json => {:status => "OK"}
   end
 
 end
