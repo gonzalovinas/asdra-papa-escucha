4db9089040aecb057474321d86e040021ffc5f80
Gonzalo Viñas <gonzalo.vinas@gmail.com>
2014-06-27 08:25:02 -0300

    .

--- app/controllers/entrevista_controller.rb
+++ app/controllers/entrevista_controller.rb
@@ -31,13 +31,13 @@ class EntrevistaController < ApplicationController
     ne.mama_domicilio   = params[:mama_domicilio]
     ne.mama_telefonos   = params[:mama_telefonos]
     ne.mama_correo      = params[:mama_correo]
-    ne.sexo             = params[:sexo]
+    ne.sexo             = params[:sexo][0].upcase
     ne.fecha_nacimiento = params[:fecha_nacimiento]
     ne.institucion_nacimiento = params[:institucion_nacimiento]
     ne.observacion_institucion= params[:observacion_institucion]
     ne.meses_nacimiento = params[:meses_nacimiento]
-    ne.parto_normal     = params[:parto_normal]
-    ne.sabia_sdown      = params[:sabia_sdown]
+    ne.parto_normal     = params[:parto_normal]? 'S' : 'N'
+    ne.sabia_sdown      = params[:sabia_sdown]? 'S' : 'N'
     ne.recepcion_flia   = params[:recepcion_flia]
     ne.patologia_agregada = params[:patologia_agregada]
     ne.observaciones    = params[:observaciones]
