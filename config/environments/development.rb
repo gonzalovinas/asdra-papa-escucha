21c988720140916bb9456a8e90c6d0f6d0c2cd6a
Gonzalo Viñas <gonzalo.vinas@gmail.com>
2014-08-04 08:22:05 -0300

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_controller.relative_url_root = '/pape'
 
 end
